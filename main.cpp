691a2b0ddbfa3885bd95f31400059fbc442f6d1d
StarukhDaniil <starukh.danil@gmail.com>
2024-10-26 09:59:31 +0300

    Added greetings for user

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <string>
 
 int main() {
+	std::string name;
+	std::cout << "What is your name?";
+	std::cin >> name;
 	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello, " << name << std::endl;
 	return 0;
 }
